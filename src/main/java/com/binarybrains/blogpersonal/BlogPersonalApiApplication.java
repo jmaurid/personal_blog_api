6d172f34c49103ba98e99ea4c7a83ce175613797
jmaurid <jmaurid@hotmail.com>
2025-06-18 22:35:09 -0600

    Initial commit

---
+++ src/main/java/com/binarybrains/blogpersonal/BlogPersonalApiApplication.java
@@ -0,0 +1,12 @@
+package com.binarybrains.blogpersonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPersonalApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BlogPersonalApiApplication.class, args);
+  }
+}
---
+++ src/test/java/com/binarybrains/blogpersonal/BlogPersonalApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.binarybrains.blogpersonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPersonalApiApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
